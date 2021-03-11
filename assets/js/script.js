4bbf5ffe6fa4c2c471dc717cde7f493215741ccf
William Nicholson <will98nicholson@gmail.com>
2021-03-11 18:25:59 -0500

    mood variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,7 @@ var weather = {
     },
 };
 var moods = {
-    userinput: {key1: value1, key2: value2},
+    userinput: { key1: value1, key2: value2 },
     happy: {
         genres: ["pop"],
     },
@@ -18,23 +18,31 @@ var moods = {
         keywords: []
     }
 }
+
+var happy = $('.smileBtn')
+var angry = $('.angryBtn')
+var love = $('.loveBtn')
+var sad = $('.sadBtn')
+var crazy = $('.crazyBtn)
+var chill = $('.chillBtn')
+
 var userInput = "happy";
 var keywords = moods[userInput].keywords;
 
 function getWeather() {
     fetch("https://community-open-weather-map.p.rapidapi.com/weather?q=London%2Cuk&lat=0&lon=0&callback=test&id=2172797&lang=null&units=%22metric%22%20or%20%22imperial%22&mode=xml%2C%20html", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "7463f1062fmsh1fe8735365773c9p140f00jsne6b9b6acaa80", // this doesn't shield private API keys! Don't care...
-		"x-rapidapi-host": "community-open-weather-map.p.rapidapi.com"
-	}
-})
-.then(response => {
-	console.log(response); // change this output location
-})
-.catch(err => {
-	console.error(err); // change this error handling output
-});
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-key": "7463f1062fmsh1fe8735365773c9p140f00jsne6b9b6acaa80", // this doesn't shield private API keys! Don't care...
+            "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com"
+        }
+    })
+        .then(response => {
+            console.log(response); // change this output location
+        })
+        .catch(err => {
+            console.error(err); // change this error handling output
+        });
 }
 
 function getMusic(mood) {
