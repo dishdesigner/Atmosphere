cd2777251fa14aa2c03cadf12fdb13a8c5b65c83
Michael Beebe <beebe79@gmail.com>
2021-03-12 12:38:49 -0500

    added video and css

--- assets/js/script.js
+++ assets/js/script.js
@@ -66,83 +66,83 @@ function getMusic(mood) {
 }
 getMusic(userInput)
 // name variables for music player
-var nowPlaying = $('.nowPlaying');
-var trackArt = $('.trackArt');
-var trackName = $('.trackName');
-var trackArtist = $('.trackArtist');
-var playpauseBtn = $('.playpausetrack');
-var nextBtn = $('.nexttrack');
-var prevBtn = $('.prevtrack');
-var currentTime = $('.current-time');
-var duration = $('.totalDuration');
-var trackIndex = 0;
-var isPlaying = false;
-var updateTimer;
-var currentTrack = document.createElement('audio');
+// var nowPlaying = $('.nowPlaying');
+// var trackArt = $('.trackArt');
+// var trackName = $('.trackName');
+// var trackArtist = $('.trackArtist');
+// var playpauseBtn = $('.playpausetrack');
+// var nextBtn = $('.nexttrack');
+// var prevBtn = $('.prevtrack');
+// var currentTime = $('.current-time');
+// var duration = $('.totalDuration');
+// var trackIndex = 0;
+// var isPlaying = false;
+// var updateTimer;
+// var currentTrack = document.createElement('audio');
 
-var trackList = [
-    path
+// var trackList = [
+//     path
 
-];
-
-
+// ];
 
 
-// clears the duration from previous track on new track load
-function loadTrack(trackIndex) {
-    clearInterval(updateTimer);
-    resetValues();
-    console.log(trackList);
-    // Loads a new track
-    currentTrack.src = trackList[trackIndex].path;
-    currentTrack.load();
 
-    // update timer
-    updateTimer = setInterval(seekUpdate, 1000);
 
-    // move to next track when song ends
-    currentTrack.addEventListener('ended', nextTrack);
-}
+// // clears the duration from previous track on new track load
+// function loadTrack(trackIndex) {
+//     clearInterval(updateTimer);
+//     resetValues();
+//     console.log(trackList);
+//     // Loads a new track
+//     currentTrack.src = trackList[trackIndex].path;
+//     currentTrack.load();
 
-$('.playpausetrack').click(function playpauseTrack() {
-    if (!isPlaying) playTrack();
-    else pauseTrack();
-},
-    function playTrack() {
-        currentTrack.play();
-        isPlaying = true;
+//     // update timer
+//     updateTimer = setInterval(seekUpdate, 1000);
 
-        playpauseBtn.innerHTML = '<i class = "fa fa-pause-circle fa-2x"><i/>';
+//     // move to next track when song ends
+//     currentTrack.addEventListener('ended', nextTrack);
+// }
 
-    },
-    $('.playpausetrack').click(function pauseTrack() {
-        currentTrack.pause(trackIndex);
-        isPlaying = false;
+// $('.playpausetrack').click(function playpauseTrack() {
+//     if (!isPlaying) playTrack();
+//     else pauseTrack();
+// },
+//     function playTrack() {
+//         currentTrack.play();
+//         isPlaying = true;
 
-        playpauseBtn.innerHTML = '<i class = "fa fa-play-circle fa-2x"><i/>';
+//         playpauseBtn.innerHTML = '<i class = "fa fa-pause-circle fa-2x"><i/>';
 
-    },
-        $('.nexttrack').click(function nextTrack() {
-            // Go back to the first track if the 
-            // current one is the last in the track list 
-            if (trackIndex < trackList.length - 1)
-                trackIndex = +1;
-            else trackIndex = 0;
+//     },
+//     $('.playpausetrack').click(function pauseTrack() {
+//         currentTrack.pause(trackIndex);
+//         isPlaying = false;
 
-            // Load and play the new track 
-            loadTrack(trackIndex);
-            playTrack();
-        },
+//         playpauseBtn.innerHTML = '<i class = "fa fa-play-circle fa-2x"><i/>';
 
-            $('.prevtrack').click(function prevTrack() {
-                // Go back to the last track if the 
-                // current one is the first in the track list 
-                if (trackIndex > 0)
-                    trackIndex = -1;
-                else trackIndex = trackList.length;
+//     },
+//         $('.nexttrack').click(function nextTrack() {
+//             // Go back to the first track if the 
+//             // current one is the last in the track list 
+//             if (trackIndex < trackList.length - 1)
+//                 trackIndex = +1;
+//             else trackIndex = 0;
 
-                // Load and play the new track 
-                loadTrack(trackIndex);
-                playTrack();
-            }))));
-loadTrack(trackIndex);
+//             // Load and play the new track 
+//             loadTrack(trackIndex);
+//             playTrack();
+//         },
+
+//             $('.prevtrack').click(function prevTrack() {
+//                 // Go back to the last track if the 
+//                 // current one is the first in the track list 
+//                 if (trackIndex > 0)
+//                     trackIndex = -1;
+//                 else trackIndex = trackList.length;
+
+//                 // Load and play the new track 
+//                 loadTrack(trackIndex);
+//                 playTrack();
+//             }))));
+// loadTrack(trackIndex);
